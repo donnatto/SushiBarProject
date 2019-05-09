5b1e1b401b9904f813394b845604ab60fed4a236
Edwin Dominguez <50028704+donnatto@users.noreply.github.com>
2019-05-08 23:23:11 -0500

    Add files via upload

---
+++ web/resources/js/main.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	"use strict";
+	$(window).scroll(function(){
+		$("nav").toggleClass("scrolled", $(this).scrollTop() > 630);
+	});//cambiar de color nav
+	
+	$(".img-hover").mouseover(function(){
+		$(".img-hover img").css("filter", "blur(10px)");
+		$(this).find("img").css("filter","blur(0px)");
+	});
+	$(".img-hover").mouseout(function(){
+		$(".img-hover img").css("filter","blur(0px)");
+	});//enfocar imagenes galeria
+	
+	
+	
+	
+	
+});
