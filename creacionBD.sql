8ead414536b34deb31c3384cc9302473bd5d26ec
Edwin Dominguez <50028704+donnatto@users.noreply.github.com>
2019-05-30 10:37:20 -0500

    script bd

---
+++ creacionBD.sql
@@ -0,0 +1,48 @@
+create database hamachiSushi
+go
+
+use hamachiSushi
+go
+create table Reserva(
+	id int identity not null,
+	nombre varchar(30) not null,
+	apellidos varchar(30) not null,
+	fecha date not null,
+	hora varchar(4) not null,
+	comensales int not null,
+	nro_Contacto varchar(15) not null,
+	correo varchar(30) not null
+	)
+go
+
+create table Usuario(
+	id int identity not null,
+	nombre varchar(30) not null,
+	apellidos varchar(30) not null,
+	correo varchar(30) not null,
+	area varchar(30) not null,
+	contraseña varchar(30) not null
+)
+go
+drop table Postulante
+go
+create table Postulante(
+	id int identity not null,
+	nombre varchar(30) not null,
+	correo varchar(30) not null,
+	nro_Contacto varchar(15) not null,
+	descripcion varchar(100) not null
+)
+go
+create table Empleado(
+	codigoEmpleado varchar(10) not null,
+	nombre varchar(30) not null,
+	apellidos varchar(30) not null,
+)
+create table Contacto(
+	idContacto int identity not null,
+	nombre varchar(30) not null,
+	correo varchar(30) not null,
+	asunto varchar(20) not null,
+	mensaje varchar(200) not null
+)
